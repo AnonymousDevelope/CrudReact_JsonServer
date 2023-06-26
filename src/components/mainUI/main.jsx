6f81001ef7d95b9af76e4833e148f81248390786
Your Name <you@example.com>
2023-06-26 09:39:20 +0500

    --save

--- src/components/mainUI/main.jsx
+++ src/components/mainUI/main.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
-
+import DB from "../../db.json"
+import $ from 'jquery'
+import("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js")
 const Main = () => {
+const heightBlock = $(".block").innerHeight();
+if(heightBlock<200){
+$(".block").css("height","200px");
+}else{
+$(".block").css("height", "auto");
+}
   return (
     <>
-    <div className="container main">
+      <div className="container main">
         <div className="row">
-            <div className="col-md blockChain">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur numquam illum optio, soluta laboriosam culpa perspiciatis explicabo aspernatur voluptate reprehenderit voluptatibus ducimus et aliquam, itaque quaerat. Nesciunt quas beatae voluptatem?</div>
-            <div className="col-md blockChain">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum maiores sequi laborum eligendi esse eos reprehenderit vel voluptatum. Labore tempora cupiditate eum. Harum quis ut voluptatum debitis reiciendis inventore eaque?</div>
+          {
+            DB.map((data, i) => {
+              return (
+                <div key={i} className="block d-flex col-md-6">
+                  <div className="blockChain w-100">
+                    <div className="title">{data.title}</div>
+                    <div className="main-part">
+                      {data.inform}
+                      <a href={data.telegram} className="nav-link fs-3"><i className="fa-brands fa-telegram"></i></a>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
-    </div>
+      </div>
     </>
   )
-}
 
+}
 export default Main
